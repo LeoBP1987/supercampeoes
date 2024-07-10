c8e0fa559b46b72c3b711800a6ba3f143f3e8fe7
Leonardo <leonardobp1987@gmail.com>
2024-07-10 18:03:49 -0300

    Tentando mudar o init_script para extensão .sh

--- init_script.sh
+++ init_script.sh
@@ -1,9 +1,10 @@
-@echo off
-REM Instalar as dependências
+#!/bin/sh
+
+# Instalar as dependências
 pip install -r requirements.txt
 
-REM Coletar arquivos estáticos
+# Coletar arquivos estáticos
 python manage.py collectstatic --noinput
 
-REM Aplicar migrações
+# Aplicar migrações
 python manage.py migrate
