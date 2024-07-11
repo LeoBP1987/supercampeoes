b7b07e788b9ed097973adbfaf7d6d7b71d347e64
Leonardo <leonardobp1987@gmail.com>
2024-07-11 14:42:08 -0300

    desfazendo a ultima mudança

--- init_script.sh
+++ init_script.sh
@@ -4,10 +4,10 @@
 pip install -r requirements.txt
 
 # Coletar arquivos estáticos
-#python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 # Aplicar migrações
-#python manage.py migrate
+python manage.py migrate
 
 # Criar superusuário
-#echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
